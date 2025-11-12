2208dc244c28b800a1dfd652eb43ba9ebfb8695b
ScratchStack <122732007+ScrachStack@users.noreply.github.com>
2025-11-12 18:58:46 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'syncsurreal'
+author 'ScratchStack (Sync Studio)'
+description 'SurrealDB Cloud wrapper for FiveM (Lua 5.4)'
+version '1.0.0'
+
+server_scripts {
+    'server/*'
+}
